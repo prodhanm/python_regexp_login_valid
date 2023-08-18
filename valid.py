67c1aa0516dd17169fa1f2f2cd2a43168736f71b
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-18 12:57:34 -0400

    added a password validation to the code.

--- valid.py
+++ valid.py
@@ -2,14 +2,24 @@ import re
 
 email = re.compile(r"^\S+@\S+\.\S+$")
 string = str(input("Enter a valid email address: "))
+password = re.compile(r"^[A-Z][a-z]+\d\d\W$")
+str2 = input("Enter your password: ")
 
 def validation(email,string):
     go_email = email.findall(string)
     return go_email
     
+
+def pass_valid(password,str2):
+    go_pass = password.findall(str2)
+    return go_pass
+
 def main():
     result = "".join(validation(email,string))
     print(result)
+    result2 = "".join(pass_valid(password,str2))
+    result_ano = len(result2) * "*"
+    print(result_ano)
 
 if __name__ == "__main__":
     main()
